073c983dd6fae9fabd081166d841b07d29a77f5a
yanglh <javaylh@163.com>
2019-08-07 15:28:48 +0800

    1.增加了两个模块

---
+++ dawn-master/dawn-common/src/main/java/com/dawn/common/DawnCommonApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawnCommonApplication.class, args);
+	}
+
+}
